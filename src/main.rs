843f5e653f9c50ea97613744dbee059b2e5f7a27
James Uther <jbu@users.noreply.github.com>
2021-12-01 20:20:27 +0000

    done

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,32 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn ans(l: Vec<i32>) -> i32 {
+fn ans_1_1(l: Vec<i32>) -> i32 {
   let tail = l.iter().zip(&l[1..]).map(|x| (x.0 < x.1) as i32);
   return tail.sum();
+}
+
+fn ans_1_2(l: Vec<i32>) -> i32 {
+  let a = l.iter();
+  let b = &l[1..];
+  let c = &l[2..];
+  let windows: Vec<i32> = a.zip(b)
+              .zip(c)
+              .map(|((x,y),z)| [x, y, z])
+              .map(|x| x[0]+x[1]+x[2])
+              .collect();
+  let drops = windows.iter()
+                     .zip(&windows[1..])
+                     .map(|x| (x.0 < x.1) as i32)
+                     .sum();
+  return drops;
 }
 fn main() {
     // File hosts must exist in current path before this produces output
     //let t = vec![199,200,208,210,200,207,240,269,260,263];
     let l = lines_from_file("./test_1.txt");
     let t: Vec<_> = l.iter().map(|x| x.parse::<i32>().unwrap()).collect();
-    let v = ans(t);
+    let v = ans_1_2(t);
     println!("{}", v);
 }
 
