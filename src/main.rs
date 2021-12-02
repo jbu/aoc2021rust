686539587392d240fcd505b8a4ef054091747fdf
James Uther <jbu@users.noreply.github.com>
2021-12-02 09:09:29 +0000

    2

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,50 @@ fn ans_1_2(l: Vec<i32>) -> i32 {
                      .sum();
   return drops;
 }
+
+fn ans_2_1(commands: Vec<&str>) -> i32 {
+  let mut h: i32 = 0;
+  let mut d: i32 = 0;
+  for command in commands {
+    let sp: Vec<&str> = command.split(" ").collect();
+    let c: (&str, &str) = (sp[0], sp[1]);
+    match c {
+      ("forward", x) => h += x.parse::<i32>().unwrap(),
+      ("up", x)      => d -= x.parse::<i32>().unwrap(),
+      ("down", x)    => d += x.parse::<i32>().unwrap(),
+      _              => panic!("invalid command"),
+    }
+  }
+  return h*d;
+}
+
+fn ans_2_2(commands: Vec<&str>) -> i32 {
+  let mut h: i32 = 0;
+  let mut d: i32 = 0;
+  let mut a: i32 = 0;
+  for command in commands {
+    let sp: Vec<&str> = command.split(" ").collect();
+    let c: (&str, &str) = (sp[0], sp[1]);
+    match c {
+      ("forward", x) => {
+        h += x.parse::<i32>().unwrap();
+        d = d + a * x.parse::<i32>().unwrap();
+      },
+      ("up", x)      => a -= x.parse::<i32>().unwrap(),
+      ("down", x)    => a += x.parse::<i32>().unwrap(),
+      _              => panic!("invalid command"),
+    }
+  }
+  return h*d;
+}
+
 fn main() {
     // File hosts must exist in current path before this produces output
     //let t = vec![199,200,208,210,200,207,240,269,260,263];
-    let l = lines_from_file("./test_1.txt");
-    let t: Vec<_> = l.iter().map(|x| x.parse::<i32>().unwrap()).collect();
-    let v = ans_1_2(t);
+    //let t: Vec<_> = l.iter().map(|x| x.parse::<i32>().unwrap()).collect();
+    let t: Vec<&str> = vec!("forward 5","down 5","forward 8","up 3","down 8","forward 2");
+    //let l = lines_from_file("./data_2.txt");
+    let v = ans_2_2(t);
     println!("{}", v);
 }
 
